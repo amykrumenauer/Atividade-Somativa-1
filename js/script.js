51af80e0c2f720d8b56bed1ee17f92a3111094c4
Amanda Krumenauer <159960338+amykrumenauer@users.noreply.github.com>
2024-11-05 11:05:34 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,28 +1,30 @@
+// Validação do formulário
 document.getElementById('adoptionForm').onsubmit = function(event) {
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
-    const age = document.getElementById('age').value;
+    const telefone = document.getElementById('telefone').value;
     const catName = document.getElementById('catName').value;
 
-    if (!name || !email || !age || !catName) {
+    if (!name || !email || !telefone || !catName) {
         alert('Por favor, preencha todos os campos.');
         event.preventDefault(); // Impede o envio do formulário
     }
 };
 
+// Captura dos dados enviados via GET
 window.onload = function() {
     const params = new URLSearchParams(window.location.search);
     const name = params.get('name');
     const email = params.get('email');
-    const age = params.get('age');
+    const telefone = params.get('telefone');
     const catName = params.get('catName');
 
-    if (name && email && age && catName) {
+    if (name && email && telefone && catName) {
         document.getElementById('result').innerHTML = `
             <h2>Dados da Adoção:</h2>
             <p>Nome: ${name}</p>
             <p>Email: ${email}</p>
-            <p>Idade: ${age}</p>
+            <p>Idade: ${telefone}</p>
             <p>Nome do Gato: ${catName}</p>
         `;
     }
